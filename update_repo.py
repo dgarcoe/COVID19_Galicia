9b457b6f024dd7eefcf843b05235450a6b37b8e5
dgarcoe <flamealchemist86@gmail.com>
2021-01-05 11:26:17 +0100

    Uploaded data 2021-01-05

--- update_repo.py
+++ update_repo.py
@@ -1,8 +1,8 @@
 import os
 from datetime import date
 
-os.system('python3 update_data.py')
-os.system('python3 update_graphs.py')
+os.system('python3 ./update_data.py')
+os.system('python3 ./update_graphs.py')
 
 print('Pushing data to repo')
 os.system('git commit -a -m "Uploaded data '+str(date.today())+'"')
