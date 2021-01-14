c2b64a2bf8953f69a25c53bfaaaf7512c5eb96f7
dgarcoe <flamealchemist86@gmail.com>
2021-01-14 12:36:22 +0100

    Better formatting of bot messages

--- bot/covid19_galicia_bot.py
+++ bot/covid19_galicia_bot.py
@@ -26,14 +26,15 @@ emojis = {
     'ill': '\U0001F912',
     'death': '\U00002620',
     'hospital': '\U0001F6CC',
-    'uci': '\U0001F6A8'
+    'uci': '\U0001F6A8',
+    'total': '\U0001F4CA'
 }
 
 commands = { 
     'help'        : 'Devolve información dos comandos dispoñibles',
-    'getKpis'     : 'Devolve o estado dos indicadores principais no último día (casos totais, casos activos, altas e falecidos)',
+    'getKpis'     : 'Devolve o estado dos indicadores principais no último día ('+emojis["total"]+'casos totais,'+emojis["ill"]+' casos activos,'+emojis["cured"]+' altas,'+emojis["death"]+'falecidos)',
     'getIA'       : 'Devolve o estado actual da Incidencia Acumulada a 7 e 14 días en toda Galicia',
-    'getHosp'     : 'Devolve o estado da ocupación hospitalaria no conxunto de Galicia',
+    'getHosp'     : 'Devolve o estado da ocupación hospitalaria no conxunto de Galicia ('+emojis["hospital"]+' hospitalizados, '+emojis["uci"]+' UCIs)',
     'getDate'     : 'Devolve a data na que se fixo a última actualización de datos',
     'getAreas'    : 'Devolve os datos de cada área sanitaria no último dia ('+emojis["cured"]+' incremento curados, '+emojis["ill"]+' incremento casos, '+emojis["death"]+' incremento falecidos, '+emojis["hospital"]+' total hospitalizados, '+emojis["uci"]+' total UCIs)'
 }
@@ -44,39 +45,40 @@ def response_main_kpis():
 
     df = pd.read_csv(data)
     kpi_text = ""
+    kpi_text += "*Indicadores principais*\n\n"
 
     new_cases = df['Total casos'].tail(1).values[0]-df['Total casos'].tail(2).values[0]
     if (new_cases>0):
-        kpi_text += emojis['nok']+' Casos totais: '+str(df['Total casos'].tail(1).values[0])+' '+emojis['up']+' Novos casos: '+str(new_cases)+'\n'
+        kpi_text += emojis['nok']+' '+emojis["total"]+str(df['Total casos'].tail(1).values[0])+'  '+emojis['up']+str(new_cases)+'\n'
     elif (new_cases==0):
-        kpi_text += emojis['ok']+' Casos totais: '+str(df['Total casos'].tail(1).values[0])+' '+emojis['same']+' Novos casos: '+str(new_cases)+'\n'
+        kpi_text += emojis['ok']+' '+emojis["total"]+str(df['Total casos'].tail(1).values[0])+'  '+emojis['same']+str(new_cases)+'\n'
 
     kpi_text += '\n'        
     active_cases = df["Total casos"].tail(1).values[0]-df["Total altas"].tail(1).values[0]-df["Total fallecidos"].tail(1).values[0]
     new_active_cases = active_cases-(df["Total casos"].tail(2).values[0]-df["Total altas"].tail(2).values[0]-df["Total fallecidos"].tail(2).values[0])
 
     if (new_active_cases>0):
-        kpi_text += emojis['nok']+' Casos activos: '+str(active_cases)+' '+emojis['up']+' Incremento activos: '+str(new_active_cases)+'\n'
+        kpi_text += emojis['nok']+' '+emojis["ill"]+str(active_cases)+'  '+emojis['up']+str(new_active_cases)+'\n'
     elif (new_active_cases<=0):
-        kpi_text += emojis['ok']+' Casos activos: '+str(active_cases)+' '+emojis['down']+' Descenso activos: '+str(new_active_cases)+'\n'
+        kpi_text += emojis['ok']+' '+emojis["ill"]+str(active_cases)+'  '+emojis['down']+str(new_active_cases)+'\n'
 
     kpi_text += '\n'
     cured = df["Total altas"].tail(1).values[0]
     new_cured = df["Total altas"].tail(1).values[0]-df["Total altas"].tail(2).values[0]
 
     if (new_cured>0):
-        kpi_text += emojis['ok']+' Altas: '+str(cured)+' '+emojis['up']+' Novas altas: '+str(new_cured)+'\n'
+        kpi_text += emojis['ok']+' '+emojis["cured"]+str(cured)+'  '+emojis['up']+str(new_cured)+'\n'
     elif(new_cured<=0):
-        kpi_text += emojis['nok']+' Altas: '+str(cured)+' '+emojis['up']+' Novas altas: '+str(new_cured)+'\n'
+        kpi_text += emojis['nok']+' '+emojis["cured"]+str(cured)+'  '+emojis['up']+str(new_cured)+'\n'
     
     kpi_text += '\n'
     deceased = df["Total fallecidos"].tail(1).values[0]
     new_deceased = df["Total fallecidos"].tail(1).values[0]-df["Total fallecidos"].tail(2).values[0]
 
     if (new_deceased>0):
-        kpi_text += emojis['nok']+' Falecidos: '+str(deceased)+' '+emojis['up']+' Novos falecidos: '+str(new_deceased)+'\n'
+        kpi_text += emojis['nok']+' '+emojis["death"]+str(deceased)+'    '+emojis['up']+str(new_deceased)+'\n'
     elif(new_deceased==0):
-        kpi_text += emojis['ok']+' Falecidos: '+str(deceased)+' '+emojis['same']+' Novos falecidos: '+str(new_deceased)+'\n'
+        kpi_text += emojis['ok']+' '+emojis["death"]+str(deceased)+'    '+emojis['same']+str(new_deceased)+'\n'
 
     return kpi_text
 
@@ -84,6 +86,8 @@ def response_IA():
 
     df = pd.read_csv(data)
     ia_text = ""
+
+    ia_text += "*Incidencia Acumulada*\n\n"
 
     ia_14 = round(df['IA 14'].tail(1).values[0],2)
     ia_7 = round(df['IA 7'].tail(1).values[0],2)
@@ -115,12 +119,14 @@ def response_date():
 
     df = pd.read_csv(data)
 
-    return emojis['calendar']+' '+df["Fecha"].tail(1).values[0]
+    return "*Última actualización*\n\n"+emojis['calendar']+' '+df["Fecha"].tail(1).values[0]
 
 def response_hospital():
 
     df = pd.read_csv(data)
     hosp_text =""
+
+    hosp_text += "*Datos de hospitalización*\n\n"
 
     hosp = df['Hospitalizados'].tail(1).values[0]
     dif_hosp = df['Hospitalizados'].tail(1).values[0]-df['Hospitalizados'].tail(2).values[0]
@@ -128,16 +134,16 @@ def response_hospital():
     dif_uci = df['UCI'].tail(1).values[0]-df['UCI'].tail(2).values[0]
 
     if (dif_hosp>0):
-        hosp_text += emojis['nok']+" Hospitalizados: "+str(hosp)+' '+emojis['up']+' Incremento hospitalizados: '+str(dif_hosp)
+        hosp_text += emojis['nok']+" "+emojis["hospital"]+str(hosp)+" "+emojis['up']+str(dif_hosp)
     elif(dif_hosp<=0):
-        hosp_text += emojis['ok']+" Hospitalizados: "+str(hosp)+' '+emojis['down']+' Descenso hospitalizados: '+str(dif_hosp)
+        hosp_text += emojis['ok']+" "+emojis["hospital"]+str(hosp)+" "+emojis['down']+str(dif_hosp)
 
     hosp_text += '\n\n'
 
     if (dif_uci>0):
-        hosp_text += emojis['nok']+" UCI: "+str(hosp)+' '+emojis['up']+' Incremento UCIs: '+str(dif_uci)
+        hosp_text += emojis['nok']+" "+emojis["uci"]+str(uci)+'   '+emojis['up']+str(dif_uci)
     elif(dif_uci<=0):
-        hosp_text += emojis['ok']+" UCI: "+str(hosp)+' '+emojis['down']+' Descenso UCIs: '+str(dif_uci)
+        hosp_text += emojis['ok']+" "+emojis["uci"]+str(uci)+'   '+emojis['down']+str(dif_uci)
 
     return hosp_text
 
@@ -152,7 +158,8 @@ def response_areas():
     hosp_coru = str(df["Total Hospitalizados A Coruna"].tail(1).values[0])
     uci_coru = str(df["Total UCI A Coruna"].tail(1).values[0])
 
-    area_text += "A Coruña       "+emojis["ill"]+inc_cases_coru+" "+emojis["cured"]+inc_cured_coru+" "+emojis["death"]+inc_dead_coru+"\n                        "+emojis["hospital"]+hosp_coru+" "+emojis["uci"]+uci_coru
+    area_text += "*Estadísticas por área sanitaria*\n\n"
+    area_text += "*A Coruña*       "+emojis["ill"]+inc_cases_coru+" "+emojis["cured"]+inc_cured_coru+" "+emojis["death"]+inc_dead_coru+"\n                        "+emojis["hospital"]+hosp_coru+" "+emojis["uci"]+uci_coru
     area_text += "\n\n"
 
     inc_cured_fer = str(df["Total Altas Ferrol"].tail(1).values[0]-df["Total Altas Ferrol"].tail(2).values[0])
@@ -161,7 +168,7 @@ def response_areas():
     hosp_fer = str(df["Total Hospitalizados Ferrol"].tail(1).values[0])
     uci_fer = str(df["Total UCI Ferrol"].tail(1).values[0])
 
-    area_text += "Ferrol             "+emojis["ill"]+inc_cases_fer+" "+emojis["cured"]+inc_cured_fer+" "+emojis["death"]+inc_dead_fer+"\n                        "+emojis["hospital"]+hosp_fer+" "+emojis["uci"]+uci_fer
+    area_text += "*Ferrol*             "+emojis["ill"]+inc_cases_fer+" "+emojis["cured"]+inc_cured_fer+" "+emojis["death"]+inc_dead_fer+"\n                        "+emojis["hospital"]+hosp_fer+" "+emojis["uci"]+uci_fer
     area_text += "\n\n"
 
     inc_cured_lugo = str(df["Total Altas Lugo"].tail(1).values[0]-df["Total Altas Lugo"].tail(2).values[0])
@@ -170,7 +177,7 @@ def response_areas():
     hosp_lugo = str(df["Total Hospitalizados Lugo"].tail(1).values[0])
     uci_lugo = str(df["Total UCI Lugo"].tail(1).values[0])
 
-    area_text += "Lugo               "+emojis["ill"]+inc_cases_lugo+" "+emojis["cured"]+inc_cured_lugo+" "+emojis["death"]+inc_dead_lugo+"\n                        "+emojis["hospital"]+hosp_lugo+" "+emojis["uci"]+uci_lugo
+    area_text += "*Lugo*               "+emojis["ill"]+inc_cases_lugo+" "+emojis["cured"]+inc_cured_lugo+" "+emojis["death"]+inc_dead_lugo+"\n                        "+emojis["hospital"]+hosp_lugo+" "+emojis["uci"]+uci_lugo
     area_text += "\n\n"
 
     inc_cured_pont = str(df["Total Altas Pontevedra"].tail(1).values[0]-df["Total Altas Pontevedra"].tail(2).values[0])
@@ -179,7 +186,7 @@ def response_areas():
     hosp_pont = str(df["Total Hospitalizados Pontevedra"].tail(1).values[0])
     uci_pont = str(df["Total UCI Pontevedra"].tail(1).values[0])
 
-    area_text += "Pontevedra   "+emojis["ill"]+inc_cases_pont+" "+emojis["cured"]+inc_cured_pont+" "+emojis["death"]+inc_dead_pont+"\n                        "+emojis["hospital"]+hosp_pont+" "+emojis["uci"]+uci_pont
+    area_text += "*Pontevedra*   "+emojis["ill"]+inc_cases_pont+" "+emojis["cured"]+inc_cured_pont+" "+emojis["death"]+inc_dead_pont+"\n                        "+emojis["hospital"]+hosp_pont+" "+emojis["uci"]+uci_pont
     area_text += "\n\n"
 
     inc_cured_ou = str(df["Total Altas Ourense"].tail(1).values[0]-df["Total Altas Ourense"].tail(2).values[0])
@@ -188,7 +195,7 @@ def response_areas():
     hosp_ou = str(df["Total Hospitalizados Ourense"].tail(1).values[0])
     uci_ou = str(df["Total UCI Ourense"].tail(1).values[0])
 
-    area_text += "Ourense        "+emojis["ill"]+inc_cases_ou+" "+emojis["cured"]+inc_cured_ou+" "+emojis["death"]+inc_dead_ou+"\n                        "+emojis["hospital"]+hosp_ou+" "+emojis["uci"]+uci_ou
+    area_text += "*Ourense*        "+emojis["ill"]+inc_cases_ou+" "+emojis["cured"]+inc_cured_ou+" "+emojis["death"]+inc_dead_ou+"\n                        "+emojis["hospital"]+hosp_ou+" "+emojis["uci"]+uci_ou
     area_text += "\n\n"
 
     inc_cured_sant = str(df["Total Altas Santiago"].tail(1).values[0]-df["Total Altas Santiago"].tail(2).values[0])
@@ -197,7 +204,7 @@ def response_areas():
     hosp_sant = str(df["Total Hospitalizados Santiago"].tail(1).values[0])
     uci_sant = str(df["Total UCI Santiago"].tail(1).values[0])
 
-    area_text += "Santiago        "+emojis["ill"]+inc_cases_sant+" "+emojis["cured"]+inc_cured_sant+" "+emojis["death"]+inc_dead_sant+"\n                        "+emojis["hospital"]+hosp_sant+" "+emojis["uci"]+uci_sant
+    area_text += "*Santiago*        "+emojis["ill"]+inc_cases_sant+" "+emojis["cured"]+inc_cured_sant+" "+emojis["death"]+inc_dead_sant+"\n                        "+emojis["hospital"]+hosp_sant+" "+emojis["uci"]+uci_sant
     area_text += "\n\n"
 
     inc_cured_vigo = str(df["Total Altas Vigo"].tail(1).values[0]-df["Total Altas Vigo"].tail(2).values[0])
@@ -206,7 +213,7 @@ def response_areas():
     hosp_vigo = str(df["Total Hospitalizados Vigo"].tail(1).values[0])
     uci_vigo = str(df["Total UCI Vigo"].tail(1).values[0])
 
-    area_text += "Vigo                "+emojis["ill"]+inc_cases_vigo+" "+emojis["cured"]+inc_cured_vigo+" "+emojis["death"]+inc_dead_vigo+"\n                        "+emojis["hospital"]+hosp_vigo+" "+emojis["uci"]+uci_vigo
+    area_text += "*Vigo*                "+emojis["ill"]+inc_cases_vigo+" "+emojis["cured"]+inc_cured_vigo+" "+emojis["death"]+inc_dead_vigo+"\n                        "+emojis["hospital"]+hosp_vigo+" "+emojis["uci"]+uci_vigo
 
     return area_text
 
@@ -233,35 +240,35 @@ def command_help(m):
 def command_kpis(m):
     logger.info("/getKpis command from "+str(m.from_user.id)+" "+m.from_user.first_name)
     cid = m.chat.id
-    bot.send_message(cid, response_main_kpis()) 
+    bot.send_message(cid, response_main_kpis(),parse_mode="markdown") 
 
 # IA page
 @bot.message_handler(commands=['getIA'])
 def command_ia(m):
     logger.info("/getIA command from "+str(m.from_user.id)+" "+m.from_user.first_name)
     cid = m.chat.id
-    bot.send_message(cid, response_IA()) 
+    bot.send_message(cid, response_IA(),parse_mode="markdown") 
 
 # Date page
 @bot.message_handler(commands=['getDate'])
 def command_date(m):
     logger.info("/getDate command from "+str(m.from_user.id)+" "+m.from_user.first_name)
     cid = m.chat.id
-    bot.send_message(cid, response_date()) 
+    bot.send_message(cid, response_date(),parse_mode="markdown") 
 
 # Hospital page
 @bot.message_handler(commands=['getHosp'])
 def command_hosp(m):
     logger.info("/getHosp command from "+str(m.from_user.id)+" "+m.from_user.first_name)
     cid = m.chat.id
-    bot.send_message(cid, response_hospital()) 
+    bot.send_message(cid, response_hospital(),parse_mode="markdown") 
 
 # Areas page
 @bot.message_handler(commands=['getAreas'])
 def command_areas(m):
     logger.info("/getAreas command from "+str(m.from_user.id)+" "+m.from_user.first_name)
     cid = m.chat.id
-    bot.send_message(cid, response_areas())
+    bot.send_message(cid, response_areas(),parse_mode="markdown")
 
 
 print('COVID-19 Galicia Telegram Bot started and waiting for messages!')
