4237c528a0f902f5229527e553516f270a98e8f1
dgarcoe <dannn86m@gmail.com>
2022-01-13 17:07:21 +0100

    Added data and updated dataset column name

--- update_data.py
+++ update_data.py
@@ -20,10 +20,10 @@ r = requests.get(url)
 with open(filename, 'wb') as f:
     f.write(r.content)
     print(filename+' downloaded')
-    
+
 print('Files saved')
 
-print('Adding new data')   
+print('Adding new data')
 
 df_day = pd.read_csv(filename,thousands='.')
 
@@ -38,7 +38,7 @@ df_day_galicia = df_day[df_day['Area_Sanitaria']=='GALICIA']
 new_total_cases = df_day_galicia['Casos_Totais'].values[0]
 new_total_recovered = df_day_galicia['Pacientes_Con_Alta'].values[0]
 new_total_deaths = df_day_galicia['Exitus'].values[0]
-new_pcr = df_day_galicia['Casos_Confirmados_PDIA_Ultimas24h'].values[0]
+new_pcr = df_day_galicia['Novos_Casos_Abertos_Ultimas24h'].values[0]
 new_cases = new_total_cases-df['Total casos'].tail(1).values[0]
 new_deaths = new_total_deaths-df['Total fallecidos'].tail(1).values[0]
 new_letality_rate = new_total_deaths*100/new_total_cases
@@ -141,7 +141,7 @@ df_new_row = pd.DataFrame([[new_days, today.strftime("%d/%m/%Y"), new_total_case
                            new_pcr, new_cases, new_deaths, new_letality_rate, new_total_in_hospital, new_total_intensive_care,
                            new_in_hospital, new_intensive_care, new_IA_14, new_IA_7, new_total_cases_coruna, new_total_recovered_coruna,
                            new_total_deaths_coruna, new_total_in_hospital_coruna, new_total_intensive_care_coruna,
-                           new_total_pcr_coruna, new_IA_14_coru, new_IA_7_coru, new_total_cases_ferrol, 
+                           new_total_pcr_coruna, new_IA_14_coru, new_IA_7_coru, new_total_cases_ferrol,
                             new_total_recovered_ferrol, new_total_deaths_ferrol,
                            new_total_in_hospital_ferrol, new_total_intensive_care_ferrol, new_total_pcr_ferrol,
                             new_IA_14_ferrol, new_IA_7_ferrol,
@@ -151,7 +151,7 @@ df_new_row = pd.DataFrame([[new_days, today.strftime("%d/%m/%Y"), new_total_case
                            new_total_deaths_ourense, new_total_in_hospital_ourense, new_total_intensive_care_ourense,
                            new_total_pcr_ourense, new_IA_14_ourense, new_IA_7_ourense,
                             new_total_cases_ponte, new_total_recovered_ponte, new_total_deaths_ponte,
-                           new_total_in_hospital_ponte, new_total_intensive_care_ponte, new_total_pcr_ponte, 
+                           new_total_in_hospital_ponte, new_total_intensive_care_ponte, new_total_pcr_ponte,
                             new_IA_14_ponte, new_IA_7_ponte, new_total_cases_sant,
                            new_total_recovered_sant, new_total_deaths_sant, new_total_in_hospital_sant, new_total_intensive_care_sant,
                            new_total_pcr_sant, new_IA_14_sant, new_IA_7_sant,
@@ -160,7 +160,7 @@ df_new_row = pd.DataFrame([[new_days, today.strftime("%d/%m/%Y"), new_total_case
                            new_IA_14_vigo, new_IA_7_vigo, new_pop_galicia, new_pop_coru, new_pop_ferrol, new_pop_lugo,
                            new_pop_ourense, new_pop_ponte, new_pop_sant, new_pop_vigo]],columns=df.keys(),
                          index=[new_days])
-    
+
 
 print('New data added')
 
